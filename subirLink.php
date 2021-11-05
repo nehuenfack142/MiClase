79bcd261b8c792987081181c3a1634532cdfcae6
Josue-Luna <josueluna650@gmail.com>
2021-11-04 21:33:27 -0300

    Subir_Link nuevo

--- subirLink.php
+++ subirLink.php
@@ -5,11 +5,12 @@ if (isset($_POST['guardar'])) {
     if (strlen($_POST['link']) >= 1) {
 	    $link = trim($_POST['link']);
 	    $cursos = trim($_POST['curso']);
-	    $consulta = "INSERT INTO link(link,curso) VALUES ('$link','$cursos')";
+	    $materia = trim($_POST['materia']);
+	    $consulta = "INSERT INTO link(link,curso,materias) VALUES ('$link','$cursos','$materia')";
 	    $resultado = mysqli_query($conex,$consulta);
 	    if ($resultado) {
 	    	?> 
-	    	<p>¡Se ha subido correctamente!</p>
+	    
            <?php
 	    } else {
 	    	?> 
