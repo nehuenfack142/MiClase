f56f9ae05232d6515317ab05a28edf1423037ce1
Josue Luna <josueluna650@gmail.com>
2021-11-05 16:53:53 -0300

    intento alumnos materias

---
+++ materias.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>materias</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+<link rel="stylesheet" href="materias.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 3.24.3, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+    <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700">
+    
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "Site1",
+		"logo": "images/LOGO.png",
+		"sameAs": []
+}</script>
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="materias">
+    <meta property="og:description" content="">
+    <meta property="og:type" content="website">
+  </head>
+  <body class="u-body"><header class="u-clearfix u-custom-color-3 u-header" id="sec-ee9f" data-animation-name="" data-animation-duration="0" data-animation-delay="0" data-animation-direction=""><div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+        <a href="Index.php" class="u-image u-logo u-image-1" data-image-width="2150" data-image-height="1280">
+          <img src="images/LOGO.png" class="u-logo-image u-logo-image-1" data-image-width="96">
+        </a>
+        <a href="Register.php" class="u-border-none u-btn u-btn-round u-button-style u-custom-color-5 u-hover-custom-color-3 u-radius-10 u-text-custom-color-3 u-text-hover-custom-color-5 u-btn-1">Register</a>
+        <a href="login.php" class="u-border-none u-btn u-btn-round u-button-style u-custom-color-3 u-hover-custom-color-5 u-radius-10 u-text-custom-color-5 u-text-hover-custom-color-3 u-btn-2">iniciar sesion</a>
+        <a href="Index.php" data-page-id="237575894" class="u-border-none u-btn u-btn-round u-button-style u-custom-color-5 u-hover-custom-color-3 u-radius-10 u-text-custom-color-3 u-text-hover-custom-color-5 u-btn-3">volver atras</a>
+      </div></header>
+    <section class="u-clearfix u-grey-15 u-section-1" id="sec-4e91">
+      <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+        <div class="u-expanded-width u-grey-5 u-radius-15 u-shape u-shape-round u-shape-1"></div>
+        <div class="mostrarMaterias" >
+            <?php
+            include("mostrarAlumno.php"); 
+            ?><br>
+            </div>
+          <div class="u-repeater u-repeater-1">
+            <div class="u-align-center u-container-style u-custom-color-5 u-list-item u-radius-20 u-repeater-item u-shape-round u-video-cover">
+              <div class="u-container-layout u-similar-container u-container-layout-1" style="max-height: 50px"><span class="u-icon u-icon-circle u-radius-8 u-text-custom-color-3 u-icon-1"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 512 512" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-baea"></use></svg><svg class="u-svg-content" viewBox="0 0 512 512" id="svg-baea"><g><path d="m151 421c0-17.284-7.356-32.875-19.09-43.833l.006-.083c1.063-14.686 1.924-30.168 2.578-46.143 30.713 5.786 52.476 17.431 64.796 34.728 15.886 22.302 11.655 48.495 9.98 55.991-5.584 24.991 9.215 50.308 34.285 55.124 14.49 2.783 28.95 4.173 43.314 4.173 50.36 0 100.864-17.212 142.556-51.648 52.459-43.33 82.557-106.497 82.574-173.304.036-126.622-110.09-229.925-225.096-225.796-62.835 2.257-119.305 33.211-158.24 67.283-.498-4.643-1.019-9.164-1.564-13.548-2.856-22.982-6.23-41.2-10.028-54.149-2.614-8.916-8.738-29.795-26.071-29.795s-23.457 20.879-26.071 29.795c-3.798 12.949-7.171 31.167-10.028 54.149-5.74 46.185-8.901 107.289-8.901 172.056 0 42.314 1.412 84.184 4.084 121.083l.006.084c-11.734 10.958-19.09 26.549-19.09 43.833v31c0 16.262-14.196 30-31 30v30h61c49.55 0 90-41.067 90-91zm136.979-360.81c96.651-3.468 194.052 84.771 194.021 195.806-.016 57.831-26.142 112.57-71.679 150.183-45.002 37.17-103.726 52.165-161.106 41.144-8.352-1.604-12.552-10.684-10.666-19.121 3.023-13.532 7.632-48.412-14.823-79.938-17.618-24.735-47.273-40.72-88.256-47.619.348-14.693.527-45.763.527-46.323 5.845 6.811 13.587 11.714 22.444 14.087 24.021 6.437 48.706-7.907 55.114-31.82 6.422-23.968-7.853-48.691-31.82-55.113-18-4.823-35.97 1.971-46.646 15.164-.667-21.309-1.694-41.749-3.054-60.855 33.943-35.861 91.33-73.274 155.944-75.595zm-132.379 160.869c2.146-8.008 10.362-12.753 18.371-10.607 7.989 2.141 12.748 10.382 10.607 18.371-2.141 7.99-10.386 12.748-18.372 10.607-8.007-2.145-12.752-10.361-10.606-18.371zm-72.406-120.617c2.591-24.144 5.363-40.509 7.806-51.524 2.443 11.015 5.215 27.38 7.806 51.524 4.639 43.231 7.194 98.476 7.194 155.558 0 5.01-.021 10.012-.061 15h-29.878c-.04-4.988-.061-9.99-.061-15 0-57.082 2.555-112.327 7.194-155.558zm-6.649 200.548h28.91c-.514 21.183-1.389 41.784-2.604 61.191-3.833-.771-7.795-1.181-11.851-1.181s-8.018.41-11.851 1.181c-1.214-19.407-2.089-40.007-2.604-61.191zm-15.545 151.01v-31c0-16.542 13.458-30 30-30s30 13.458 30 30c0 33.635-26.916 61-60 61h-8.256c5.386-9.041 8.256-19.314 8.256-30z"></path><path d="m402.673 202.967c17.586-17.585 17.59-46.051 0-63.64-17.544-17.545-46.094-17.546-63.64 0-17.586 17.585-17.59 46.051 0 63.64 17.545 17.545 46.094 17.546 63.64 0zm-42.427-42.426c5.849-5.848 15.364-5.849 21.214 0 5.863 5.863 5.863 15.35 0 21.213-5.851 5.849-15.365 5.848-21.214 0-5.862-5.863-5.863-15.351 0-21.213z"></path><path d="m266.588 183.556c23.967-6.422 38.243-31.146 31.821-55.113-6.424-23.967-31.148-38.242-55.114-31.82-23.967 6.422-38.242 31.146-31.82 55.114 6.437 24.022 31.087 38.256 55.113 31.819zm-15.528-57.956c8.006-2.145 16.235 2.636 18.372 10.606 2.141 7.989-2.618 16.231-10.607 18.371-8.007 2.145-16.225-2.597-18.371-10.606-2.142-7.989 2.616-16.23 10.606-18.371z"></path><path d="m273.591 383.559c6.414 23.932 31.112 38.251 55.115 31.819 23.967-6.422 38.241-31.146 31.819-55.113-6.437-24.019-31.082-38.258-55.114-31.82-23.967 6.422-38.242 31.146-31.82 55.114zm39.586-26.137c8.022-2.15 16.23 2.614 18.37 10.606 2.141 7.99-2.617 16.231-10.606 18.372-7.99 2.141-16.23-2.619-18.371-10.606-2.141-7.99 2.617-16.231 10.607-18.372z"></path><path d="m358.444 275.412c-6.422 23.968 7.853 48.691 31.82 55.114 24.004 6.432 48.671-7.778 55.113-31.82 6.436-24.022-7.794-48.676-31.819-55.113-24.021-6.44-48.675 7.793-55.114 31.819zm57.955 15.529c-2.143 8.006-10.362 12.753-18.37 10.607-7.989-2.141-12.747-10.382-10.606-18.372 2.14-7.991 10.346-12.756 18.37-10.606 7.988 2.14 12.747 10.381 10.606 18.371z"></path>
+</g></svg></span>
+                <a href="https://nicepage.com/website-templates" class="u-btn u-button-style u-custom-font u-font-oswald u-text-custom-color-3 u-btn-1"> Artistica</a>
+              </div>
+            </div>
+            <div class="mostrarMaterias" >
+              <!--
+            <?php
+            include("mostrar.php"); 
+            ?><br>
+            </div>
+            -->
+            
+            
+            
+            
+            
+            
+            
+            
+            </div>
+          </div>
+        </div>
+      </div>
+        <div class="u-layout-grid u-list u-list-1">
+    </section>
+    
+    
+    <footer class="u-clearfix u-custom-color-3 u-footer" id="sec-8a17"><div class="u-clearfix u-sheet u-sheet-1">
+        <div class="u-align-left u-social-icons u-spacing-10 u-social-icons-1">
+          <a class="u-social-url" title="facebook" target="_blank" href=""><span class="u-icon u-icon-circle u-social-facebook u-social-icon u-icon-1"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-86d7"></use></svg><svg class="u-svg-content" viewBox="0 0 112 112" x="0" y="0" id="svg-86d7"><circle fill="currentColor" cx="56.1" cy="56.1" r="55"></circle><path fill="#FFFFFF" d="M73.5,31.6h-9.1c-1.4,0-3.6,0.8-3.6,3.9v8.5h12.6L72,58.3H60.8v40.8H43.9V58.3h-8V43.9h8v-9.2
+            c0-6.7,3.1-17,17-17h12.5v13.9H73.5z"></path></svg></span>
+          </a>
+          <a class="u-social-url" title="twitter" target="_blank" href=""><span class="u-icon u-icon-circle u-social-icon u-social-twitter u-icon-2"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-0041"></use></svg><svg class="u-svg-content" viewBox="0 0 112 112" x="0" y="0" id="svg-0041"><circle fill="currentColor" class="st0" cx="56.1" cy="56.1" r="55"></circle><path fill="#FFFFFF" d="M83.8,47.3c0,0.6,0,1.2,0,1.7c0,17.7-13.5,38.2-38.2,38.2C38,87.2,31,85,25,81.2c1,0.1,2.1,0.2,3.2,0.2
+            c6.3,0,12.1-2.1,16.7-5.7c-5.9-0.1-10.8-4-12.5-9.3c0.8,0.2,1.7,0.2,2.5,0.2c1.2,0,2.4-0.2,3.5-0.5c-6.1-1.2-10.8-6.7-10.8-13.1
+            c0-0.1,0-0.1,0-0.2c1.8,1,3.9,1.6,6.1,1.7c-3.6-2.4-6-6.5-6-11.2c0-2.5,0.7-4.8,1.8-6.7c6.6,8.1,16.5,13.5,27.6,14
+            c-0.2-1-0.3-2-0.3-3.1c0-7.4,6-13.4,13.4-13.4c3.9,0,7.3,1.6,9.8,4.2c3.1-0.6,5.9-1.7,8.5-3.3c-1,3.1-3.1,5.8-5.9,7.4
+            c2.7-0.3,5.3-1,7.7-2.1C88.7,43,86.4,45.4,83.8,47.3z"></path></svg></span>
+          </a>
+          <a class="u-social-url" title="instagram" target="_blank" href=""><span class="u-icon u-icon-circle u-social-icon u-social-instagram u-icon-3"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-f704"></use></svg><svg class="u-svg-content" viewBox="0 0 112 112" x="0" y="0" id="svg-f704"><circle fill="currentColor" cx="56.1" cy="56.1" r="55"></circle><path fill="#FFFFFF" d="M55.9,38.2c-9.9,0-17.9,8-17.9,17.9C38,66,46,74,55.9,74c9.9,0,17.9-8,17.9-17.9C73.8,46.2,65.8,38.2,55.9,38.2
+            z M55.9,66.4c-5.7,0-10.3-4.6-10.3-10.3c-0.1-5.7,4.6-10.3,10.3-10.3c5.7,0,10.3,4.6,10.3,10.3C66.2,61.8,61.6,66.4,55.9,66.4z"></path><path fill="#FFFFFF" d="M74.3,33.5c-2.3,0-4.2,1.9-4.2,4.2s1.9,4.2,4.2,4.2s4.2-1.9,4.2-4.2S76.6,33.5,74.3,33.5z"></path><path fill="#FFFFFF" d="M73.1,21.3H38.6c-9.7,0-17.5,7.9-17.5,17.5v34.5c0,9.7,7.9,17.6,17.5,17.6h34.5c9.7,0,17.5-7.9,17.5-17.5V38.8
+            C90.6,29.1,82.7,21.3,73.1,21.3z M83,73.3c0,5.5-4.5,9.9-9.9,9.9H38.6c-5.5,0-9.9-4.5-9.9-9.9V38.8c0-5.5,4.5-9.9,9.9-9.9h34.5
+            c5.5,0,9.9,4.5,9.9,9.9V73.3z"></path></svg></span>
+          </a>
+          <a class="u-social-url" title="linkedin" target="_blank" href=""><span class="u-icon u-icon-circle u-social-icon u-social-linkedin u-icon-4"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-fc14"></use></svg><svg class="u-svg-content" viewBox="0 0 112 112" x="0" y="0" id="svg-fc14"><circle fill="currentColor" cx="56.1" cy="56.1" r="55"></circle><path fill="#FFFFFF" d="M41.3,83.7H27.9V43.4h13.4V83.7z M34.6,37.9L34.6,37.9c-4.6,0-7.5-3.1-7.5-7c0-4,3-7,7.6-7s7.4,3,7.5,7
+            C42.2,34.8,39.2,37.9,34.6,37.9z M89.6,83.7H76.2V62.2c0-5.4-1.9-9.1-6.8-9.1c-3.7,0-5.9,2.5-6.9,4.9c-0.4,0.9-0.4,2.1-0.4,3.3v22.5
+            H48.7c0,0,0.2-36.5,0-40.3h13.4v5.7c1.8-2.7,5-6.7,12.1-6.7c8.8,0,15.4,5.8,15.4,18.1V83.7z"></path></svg></span>
+          </a>
+        </div>
+      </div></footer>
+    <section class="u-backlink u-clearfix u-grey-80">
+      <a class="u-link" href="politca-y-terminos.html" target="_blank">
+        <span>Terminos y condicines</span>
+      </a>
+    </section>
+
+  </body>
+</html>
---
+++ materiasAlum.php
@@ -0,0 +1,58 @@
+<?php
+$inc = include("con_db.php");
+if(!isset($_SESSION)){
+session_start();
+$seccion=$_SESSION['email'];}
+{
+$resultado = mysqli_query($conex,"SELECT * FROM datos where email ='".$seccion."'" );
+
+	if ($resultado) {
+	  		while ($row = $resultado -> fetch_array()){
+			$cursos = $row['cursos'];
+			$id = $row['id'];
+			$cons=$conex->query("SELECT id FROM datos where email ='".$email."' and id = '".$id."' ");
+			$filas= mysqli_num_rows($cons);
+			
+
+		}
+	}
+
+}
+?>
+
+
+			
+
+			<?php 
+				$tabla = mysqli_query($conex,"SELECT * FROM datos where email ='".$email."' and id = '".$id."'");
+
+				while ($row = $tabla -> fetch_array()){ ?>
+				<div class=datos2>
+					
+							<td><?php echo $row['materias'];?></td><br>
+						
+					
+			 </div>
+			<?php } ?>
+		
+
+			<style>
+				.datos2{
+				min-height: 300px;
+				max-height: 400px;
+				width: 300px;
+    			background: #89d0ca;
+    			color: #5e35b1;
+    			border-radius: 20px;
+    			font-size: 48px;
+    			float: center;
+    			padding: 150px 110px 120px 130px;
+    			font-family: Oswald;
+				}
+
+			</style>
+
+
+
+
+
