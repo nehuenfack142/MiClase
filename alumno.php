c0ef9c685bfcbb7bd378c87f027fdd22ba9a07de
Josue Luna <josueluna650@gmail.com>
2021-11-05 17:06:12 -0300

    cambios interfaz de materias de alumno

--- alumno.php
+++ alumno.php
@@ -64,7 +64,7 @@
 </g><g><path d="m473.44 324.87c4.143 0 7.5-3.358 7.5-7.5v-123.49c0-7.666-5.69-7.547-15.54-7.5v-132.28c0-4.142-3.357-7.5-7.5-7.5h-79.69c-4.143 0-7.5 3.358-7.5 7.5s3.357 7.5 7.5 7.5h72.19v124.78h-16.06l.003-85.513c0-4.142-3.357-7.5-7.5-7.5h-341.686c-4.142 0-7.5 3.358-7.5 7.5l.002 85.513h-16.059v-171.38h144.703l44.393 44.403c1.406 1.407 3.314 2.197 5.304 2.197h89.53c4.143 0 7.5-3.358 7.5-7.5s-3.357-7.5-7.5-7.5h-86.423l-44.393-44.403c-1.407-1.406-3.315-2.197-5.304-2.197h-155.31c-4.142 0-7.5 3.358-7.5 7.5v178.88c-8.836.112-15.54-.166-15.54 7.5v310.62c0 4.142 3.358 7.5 7.5 7.5h434.88c4.143 0 7.5-3.358 7.5-7.5v-154.45c0-4.142-3.357-7.5-7.5-7.5s-7.5 3.358-7.5 7.5v146.95h-419.88v-295.62h87.73c4.142 0 7.5-3.358 7.5-7.5s-3.358-7.5-7.5-7.5h-41.13v-16.07h326.68v16.07h-252.87c-4.142 0-7.5 3.358-7.5 7.5s3.358 7.5 7.5 7.5h299.47v115.99c0 4.142 3.358 7.5 7.5 7.5zm-380.783-216.503h326.685v15.853h-326.685zm0 46.916v-15.854h326.685v15.854z"></path><path d="m155.313 333.893c0 55.519 45.168 100.687 100.687 100.687 54.537 0 100.687-46.005 100.687-100.687 0-55.519-45.168-100.687-100.687-100.687s-100.687 45.168-100.687 100.687zm155.118 66.128c-31.066 25.616-78.023 25.579-109.056-.164v-33.179c0-2.941 2.138-5.614 5.339-6.705l33.205-8.372h31.968l33.204 8.372c3.202 1.09 5.34 3.764 5.34 6.705zm-77.993-86.767v-10.637c0-12.875 10.57-23.349 23.562-23.349s23.562 10.475 23.562 23.349v10.637c0 12.856-10.539 23.317-23.504 23.348h-.116c-12.966-.031-23.504-10.492-23.504-23.348zm109.248 20.639c0 18.706-6.043 36.019-16.255 50.125v-17.34c0-9.562-6.424-18.025-15.983-21.062-.145-.046-.29-.087-.437-.125l-24.707-6.229c6.361-6.841 10.257-15.979 10.257-26.01v-10.637c0-21.146-17.299-38.349-38.562-38.349s-38.562 17.203-38.562 38.349v10.637c0 10.01 3.881 19.134 10.22 25.97l-24.862 6.268c-.147.037-.292.079-.437.124-9.561 3.036-15.984 11.5-15.984 21.062v17.068c-10.094-14.057-16.062-31.266-16.062-49.854 0-47.248 38.439-85.687 85.687-85.687 47.249.003 85.687 38.442 85.687 85.69z"></path>
 </g>
 </g></svg></span>
-                    <a href="alumno.html#sec-ddd5" data-page-id="60152624" class="u-btn u-button-style u-custom-font u-font-oswald u-text-custom-color-3 u-btn-2">Carpeta</a>
+                    <a href="https://docs.google.com/document/u/0/?hl=es" data-page-id="60152624" class="u-btn u-button-style u-custom-font u-font-oswald u-text-custom-color-3 u-btn-2">Carpeta</a>
                   </div>
                 </div>
                 <div class="u-container-style u-custom-color-5 u-list-item u-radius-15 u-repeater-item u-shape-round u-list-item-3">
@@ -109,13 +109,10 @@
           <div class="u-container-layout u-container-layout-1">
             <div class="u-custom-color-5 u-expanded-width u-radius-20 u-shape u-shape-round u-shape-1"></div>
             <div class="u-custom-color-3 u-radius-20 u-shape u-shape-round u-shape-2"></div>
-            <div class="u-image u-image-circle u-image-1" alt="" data-image-width="920" data-image-height="800"></div>
-            <h6 class="u-text u-text-1">
-              <span class="u-text-white">Matematica</span>&nbsp;
+            <h6 class="u-text u-text-1" >
+              Tareas&nbsp;
             </h6>
-            <h6 class="u-text u-text-2">
-              <span class="u-text-white">Profesora: Alguien con nombre de profesora de matematica</span>
-            </h6>
+            
             <div class="u-container-style u-custom-color-5 u-group u-radius-15 u-shape-round u-group-2">
               <div class="u-container-layout u-container-layout-2">
                 <div class="u-border-4 u-border-custom-color-3 u-shape u-shape-right u-shape-top u-shape-3"></div>
@@ -165,54 +162,12 @@
         </div>
       </div>
     </section>
-    <section class="u-clearfix u-grey-15 u-section-3" id="sec-ddd5">
-      <div class="u-clearfix u-sheet u-sheet-1">
-        <div class="u-container-style u-group u-radius-20 u-shape-round u-white u-group-1">
-          <div class="u-container-layout u-container-layout-1">
-            <div class="u-custom-color-5 u-expanded-width u-radius-20 u-shape u-shape-round u-shape-1"></div>
-            <div class="u-custom-color-3 u-radius-20 u-shape u-shape-round u-shape-2"></div>
-            <div class="u-image u-image-circle u-image-1" alt="" data-image-width="920" data-image-height="800"></div>
-            <h6 class="u-text u-text-1">
-              <span class="u-text-white">Matematica</span>&nbsp;
-            </h6>
-            <h6 class="u-text u-text-2">
-              <span class="u-text-white">Profesora: Alguien con nombre de profesora de matematica</span>
-            </h6>
-            <div class="u-container-style u-group u-palette-2-base u-radius-15 u-shape-round u-group-2">
-              <div class="u-container-layout u-container-layout-2">
-                <h1 class="u-text u-text-default u-text-3">ACA SE VA A PODER ESCRIBIR LAS NOTAS COMO SI FUESE UN WORD</h1>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
     
     
-    <footer class="u-clearfix u-custom-color-3 u-footer" id="sec-8a17"><div class="u-clearfix u-sheet u-sheet-1">
-        <div class="u-align-left u-social-icons u-spacing-10 u-social-icons-1">
-          <a class="u-social-url" title="facebook" target="_blank" href=""><span class="u-icon u-icon-circle u-social-facebook u-social-icon u-icon-1"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-86d7"></use></svg><svg class="u-svg-content" viewBox="0 0 112 112" x="0" y="0" id="svg-86d7"><circle fill="currentColor" cx="56.1" cy="56.1" r="55"></circle><path fill="#FFFFFF" d="M73.5,31.6h-9.1c-1.4,0-3.6,0.8-3.6,3.9v8.5h12.6L72,58.3H60.8v40.8H43.9V58.3h-8V43.9h8v-9.2
-            c0-6.7,3.1-17,17-17h12.5v13.9H73.5z"></path></svg></span>
-          </a>
-          <a class="u-social-url" title="twitter" target="_blank" href=""><span class="u-icon u-icon-circle u-social-icon u-social-twitter u-icon-2"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-0041"></use></svg><svg class="u-svg-content" viewBox="0 0 112 112" x="0" y="0" id="svg-0041"><circle fill="currentColor" class="st0" cx="56.1" cy="56.1" r="55"></circle><path fill="#FFFFFF" d="M83.8,47.3c0,0.6,0,1.2,0,1.7c0,17.7-13.5,38.2-38.2,38.2C38,87.2,31,85,25,81.2c1,0.1,2.1,0.2,3.2,0.2
-            c6.3,0,12.1-2.1,16.7-5.7c-5.9-0.1-10.8-4-12.5-9.3c0.8,0.2,1.7,0.2,2.5,0.2c1.2,0,2.4-0.2,3.5-0.5c-6.1-1.2-10.8-6.7-10.8-13.1
-            c0-0.1,0-0.1,0-0.2c1.8,1,3.9,1.6,6.1,1.7c-3.6-2.4-6-6.5-6-11.2c0-2.5,0.7-4.8,1.8-6.7c6.6,8.1,16.5,13.5,27.6,14
-            c-0.2-1-0.3-2-0.3-3.1c0-7.4,6-13.4,13.4-13.4c3.9,0,7.3,1.6,9.8,4.2c3.1-0.6,5.9-1.7,8.5-3.3c-1,3.1-3.1,5.8-5.9,7.4
-            c2.7-0.3,5.3-1,7.7-2.1C88.7,43,86.4,45.4,83.8,47.3z"></path></svg></span>
-          </a>
-          <a class="u-social-url" title="instagram" target="_blank" href=""><span class="u-icon u-icon-circle u-social-icon u-social-instagram u-icon-3"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-f704"></use></svg><svg class="u-svg-content" viewBox="0 0 112 112" x="0" y="0" id="svg-f704"><circle fill="currentColor" cx="56.1" cy="56.1" r="55"></circle><path fill="#FFFFFF" d="M55.9,38.2c-9.9,0-17.9,8-17.9,17.9C38,66,46,74,55.9,74c9.9,0,17.9-8,17.9-17.9C73.8,46.2,65.8,38.2,55.9,38.2
-            z M55.9,66.4c-5.7,0-10.3-4.6-10.3-10.3c-0.1-5.7,4.6-10.3,10.3-10.3c5.7,0,10.3,4.6,10.3,10.3C66.2,61.8,61.6,66.4,55.9,66.4z"></path><path fill="#FFFFFF" d="M74.3,33.5c-2.3,0-4.2,1.9-4.2,4.2s1.9,4.2,4.2,4.2s4.2-1.9,4.2-4.2S76.6,33.5,74.3,33.5z"></path><path fill="#FFFFFF" d="M73.1,21.3H38.6c-9.7,0-17.5,7.9-17.5,17.5v34.5c0,9.7,7.9,17.6,17.5,17.6h34.5c9.7,0,17.5-7.9,17.5-17.5V38.8
-            C90.6,29.1,82.7,21.3,73.1,21.3z M83,73.3c0,5.5-4.5,9.9-9.9,9.9H38.6c-5.5,0-9.9-4.5-9.9-9.9V38.8c0-5.5,4.5-9.9,9.9-9.9h34.5
-            c5.5,0,9.9,4.5,9.9,9.9V73.3z"></path></svg></span>
-          </a>
-          <a class="u-social-url" title="linkedin" target="_blank" href=""><span class="u-icon u-icon-circle u-social-icon u-social-linkedin u-icon-4"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-fc14"></use></svg><svg class="u-svg-content" viewBox="0 0 112 112" x="0" y="0" id="svg-fc14"><circle fill="currentColor" cx="56.1" cy="56.1" r="55"></circle><path fill="#FFFFFF" d="M41.3,83.7H27.9V43.4h13.4V83.7z M34.6,37.9L34.6,37.9c-4.6,0-7.5-3.1-7.5-7c0-4,3-7,7.6-7s7.4,3,7.5,7
-            C42.2,34.8,39.2,37.9,34.6,37.9z M89.6,83.7H76.2V62.2c0-5.4-1.9-9.1-6.8-9.1c-3.7,0-5.9,2.5-6.9,4.9c-0.4,0.9-0.4,2.1-0.4,3.3v22.5
-            H48.7c0,0,0.2-36.5,0-40.3h13.4v5.7c1.8-2.7,5-6.7,12.1-6.7c8.8,0,15.4,5.8,15.4,18.1V83.7z"></path></svg></span>
-          </a>
-        </div>
-      </div></footer>
+    
+    
     <section class="u-backlink u-clearfix u-grey-80">
-      <a class="u-link" href="politca-y-terminos.html" target="_blank">
+      <a href= "politca-y-terminos.html"class="u-link" href="politca-y-terminos.html" target="_blank">
         <span>Terminos y condicines</span>
       </a>
     </section>
