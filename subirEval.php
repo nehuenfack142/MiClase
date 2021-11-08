4ad7b7ad2be543f4ab46adfd41abb328214e56e9
facucugliari <67327874+facucugliari@users.noreply.github.com>
2021-11-08 01:11:29 -0300

    subir link evaluaciones

--- subirEval.php
+++ subirEval.php
@@ -1,11 +1,11 @@
 <?php
 
 include("con_db.php");
-if (isset($_POST['guardar'])) {
-    if (strlen($_POST['link']) >= 1) {
-	    $link = trim($_POST['link']);
-	    $cursos = trim($_POST['curso']);
-	    $materia = trim($_POST['materia']);
+if (isset($_POST['guardar1'])) {
+    if (strlen($_POST['link1']) >= 1) {
+	    $link = trim($_POST['link1']);
+	    $cursos = trim($_POST['curso1']);
+	    $materia = trim($_POST['materia1']);
 	    $consulta = "INSERT INTO evaluaciones(link,curso,materias) VALUES ('$link','$cursos','$materia')";
 	    $resultado = mysqli_query($conex,$consulta);
 	    if ($resultado) {
