0888e10e6a84dc09cbc2bee2ec7c8a71170542dd
Josue-Luna <josueluna650@gmail.com>
2021-11-08 01:29:24 -0300

    na q ver

--- cursosProf.php
+++ cursosProf.php
@@ -35,22 +35,13 @@ $i=0;
 
 				for($i=0;$i<$filas;$i++){ ?>
 				<div class="datos2" style="margin: 0px 0px 50px 0px; ">
-<<<<<<< HEAD
 					
 							
 			                <form  action="profesor.php" method="post"  class="u-custom-color-5 u-hover-custom-color-3 u-radius-10 u-text-custom-color-3 u-text-hover-custom-color-5 u-btn-3" >
 			                <input style=" background: transparent; padding: 24px 900px 24px 50px; border-color: transparent; margin: -10px -50px -200px -40px ; " type="submit" name="enviar" value="<?php echo $cur[$i];?>"> 
 			                <input  name="enviar2"style="margin: -20px 0px 0px 65%; font-size: 40px; "value="<?php echo $mat[$i];?>">Materia:  </input><br>
 			              	</form>
-			              
-							
-=======
-				
-			               <form  action="profesor.php" method="post"  class="u-custom-color-5 u-hover-custom-color-3 u-radius-10 u-text-custom-color-3 u-text-hover-custom-color-5 u-btn-3" >
-			                <input style="background: transparent; padding: 24px 900px 24px 50px; border-color: transparent;" type="submit" name="enviar" value="<?php echo $cur[$i];?>"><br>
-			                <input style="background: transparent; margin: 0px  0px 75% ; padding: 0px -100px 0px 0px; border-color: transparent;" name="enviar2" value="<?php echo $mat[$i];?>"><br>
-							</form>
->>>>>>> origin/main
+			             
 					
 			 </div>
 			<?php } ?>
