cdf7df63a570235d2dfe8fc1c61d29eee1fe6a41
nehuen <nehuentriacca.nt56@gmail.com>
2021-11-08 00:14:04 -0300

    desarreglo del LISTO CURSO

--- cursosProf.php
+++ cursosProf.php
@@ -37,11 +37,9 @@ $i=0;
 				<div class="datos2" style="margin: 0px 0px 50px 0px; ">
 					
 							
-			                <form  action="profesor.php" method="post"  class="u-custom-color-5 u-hover-custom-color-3 u-radius-10 u-text-custom-color-3 u-text-hover-custom-color-5 u-btn-3" >
-			                <input style=" background: transparent; padding: 24px 900px 24px 50px; border-color: transparent; margin: -10px -50px -200px -40px ; " type="submit" name="enviar" value="<?php echo $cur[$i];?>"> 
-			                <imput style="margin: -20px 0px 0px 65%; font-size: 40px; ">Materia: <?php echo $mat[$i];?> </imput><br>
-			              	</form>
-			              
+			               <form  action="profesor.php" method="post"  class="u-custom-color-5 u-hover-custom-color-3 u-radius-10 u-text-custom-color-3 u-text-hover-custom-color-5 u-btn-3" >
+			                <input style="background: transparent; padding: 24px 900px 24px 50px; border-color: transparent;" type="submit" name="enviar" value="<?php echo $cur[$i];?>"><br>
+			                <input style="background: transparent; padding: 24px 900px 24px 50px; border-color: transparent;" name="enviar2" value="<?php echo $mat[$i];?>"><br>
 							
 					
 			 </div>
